57d2c23f074dca3bcf871a71090e07a3d0971fc0
hwzhuanyong <67941863+hwzhuanyong@users.noreply.github.com>
2020-10-19 11:09:35 +0800

    Update recv.go

--- core/recv.go
+++ core/recv.go
@@ -11,6 +11,11 @@ import (
 	"strings"
 	"sync/atomic"
 	"time"
+	"fmt"
+	"strings"
+	"net/http"
+	"io/ioutil"
+	
 )
 
 func Recv(device string, options *Options, flagID uint16, retryChan chan RetryStruct) {
@@ -118,6 +123,27 @@ func Recv(device string, options *Options, flagID uint16, retryChan chan RetrySt
 					}
 				} else {
 					gologger.Silentf("%s\n", msg)
+					url := "https://biu.seeksec.cn/api/domain"
+					method := "POST"
+				  
+					payload := strings.NewReader("{\"domain\":\"vivo.xyz\",\"monitor\":false,\"hosts\":\""+msg+"\"}")
+				  
+					client := &http.Client {
+					}
+					req, err := http.NewRequest(method, url, payload)
+				  
+					if err != nil {
+					  fmt.Println(err)
+					}
+					req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:81.0) Gecko/20100101 Firefox/81.0")
+					req.Header.Add("Content-Type", "application/json;charset=utf-8")
+					req.Header.Add("Biu-Api-Key", options.BiuKey)
+				  
+					res, err := client.Do(req)
+					defer res.Body.Close()
+					body, err := ioutil.ReadAll(res.Body)
+					fmt.Println(string(body))
+					
 				}
 				if isSummary {
 					AsnResults = append(AsnResults, data)
